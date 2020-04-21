9acd61a3acd175f634172c0b996e3ea1f9a197e4
babyface <58160282@go.buu.ac.th>
2020-04-22 00:15:23 +0700

    test

---
+++ src/components/MyName.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>{{ name }}</h1>
+    </div>    
+</template>
+
+<script>
+export default {
+    name: 'MyName',
+  props: {
+    name: String
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
